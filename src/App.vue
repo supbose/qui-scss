cca47659e2fa623395c5a8f337e37de072e1de64
supbose <yshxw@qq.com>
2022-07-16 11:52:16 +0800

    修复字体大小bug

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: supbose yshxw@qq.com
  * @Date: 2022-07-12 20:15:06
  * @LastEditors: supbose yshxw@qq.com
- * @LastEditTime: 2022-07-13 20:26:31
+ * @LastEditTime: 2022-07-16 10:48:18
  * @FilePath: \qui-scss\src\App.vue
  * @Description: 
  * 
@@ -11,44 +11,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted, onUpdated, onUnmounted, reactive, ref, nextTick } from "vue";
-import color from "./command/color.js";
-const colorName = reactive([
-	`blue`,
-	`cyan`,
-	`geekblue`,
-	`gold`,
-	`gray`,
-	`green`,
-	`lime`,
-	`magenta`,
-	`orange`,
-	`purple`,
-	`red`,
-	`theme`,
-	`volcano`,
-	`yellow`,
-]);
-onMounted(() => {
-	formatColor()
 
-});
-const formatColor = (v) => {
-	return color[v]
-}
-
+import quiColor from './components/quiColor.vue'
 </script>
 
 <template>
-	<div v-for="(colorItem, index) in colorName" :key="index">
-		<div class="qui-flex-row-space-around">
-			<div v-for="(item, cindex) in 10" class="qui-flex1 qui-text qui-text-center qui-text-lines-1" :key="cindex">
-				<div :class="[`bg-${colorItem}-${item}`]">
-					{{ formatColor(`${colorItem}-${item}`) }}
-				</div>
-			</div>
-		</div>
-	</div>
+	<qui-color/>
+
 </template>
 
 <style lang="scss">
---
+++ src/components/quiColor.vue
@@ -0,0 +1,40 @@
+<template>
+	<div v-for="(colorItem, index) in colorName" :key="index">
+		<div class="qui-flex-row-nowrap">
+			<div class="qui-flex1 qui-text qui-text-center qui-text-lines-1" v-for="(item, cindex) in 10" :key="cindex">
+				<div :class="[`bg-${colorItem}-${item}`]">{{ formatColor(`${colorItem}-${item}`) }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import { onMounted,reactive, } from "vue"
+	import color from "../command/color.js"
+	const colorName = reactive([
+		`blue`,
+		`cyan`,
+		`geekblue`,
+		`gold`,
+		`gray`,
+		`green`,
+		`lime`,
+		`magenta`,
+		`orange`,
+		`purple`,
+		`red`,
+		`theme`,
+		`volcano`,
+		`yellow`,
+	])
+	onMounted(() => {
+		formatColor()
+	
+	})
+	const formatColor = (v) => {
+		return color[v]
+	}
+</script>
+
+<style>
+</style>
